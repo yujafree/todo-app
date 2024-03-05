eace3eac8323b55f5fe95082d1aa07102dc07744
yujafree <yuja@gsland153.com>
2024-03-05 20:37:35 +0900

    first comit

---
+++ demo.js
@@ -0,0 +1,23 @@
+//데이터 타입
+//문자열 string "가나다"
+//number 1 10
+//boolean: 참이냐 거짓이냐 (true/false)
+true;
+false;
+//null:아무것도 없는 것
+null;
+//undefined: 정의되지 않음
+undefined ;
+//둘의 차이: null은 정의했지만 값을 정해놓지 않은 것, undefined는 정의도 하지 앟은 것
+//symble 
+
+//변수:데이터를 저장하는 공간, 주머니
+//변수를 부를 수 있는 이름만 정하면 됨
+height = 180;
+christ = true;
+
+console.log("height", height);
+
+//함수
+//조건문
+//빈복
